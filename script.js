0ba600a3a40d1a10a7162cc391b0fab61ef899fa
Kira <elizabet.krd@gmail.com>
2024-05-11 18:46:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,122 +1,83 @@
-let signs = ['+', '-', '*']
-let container_main = document.querySelector('.main')
-let container_start = document.querySelector('.start')
-let container_start_h3 = container_start.querySelector('h3')
-let question_field = document.querySelector('.question')
-let answer_buttons = document.querySelectorAll('.answer')
-let start_button = document.querySelector('.start-btn')
+const arrowBtns = document.querySelectorAll('.arrow-btn')
+const cardBtns = document.querySelectorAll('.card')
+let currentCard = 2;
+let dir = 1;
+moveCards()
 
-
-let cookie = false
-let cookies = document.cookie.split('; ')
-for (let i = 0; i < cookies.length; i += 1) {
-    if (cookies[i].split('=')[0] == 'numbers_score') {
-        cookie = cookies[i].split('=')[1]
-        break
-    }
-}
-function shuffle(array) {
-    let currentIndex = array.length,  randomIndex;
+arrowBtns.forEach((btn,i)=>{
+  btn.onpointerenter = (e)=> gsap.to(btn, {
+    ease:'expo',
+    'box-shadow':'0 3px 4px #00000050'
+  })
   
-    while (currentIndex != 0) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
-    }
+  btn.onpointerleave = (e)=> gsap.to(btn, {
+    ease:'expo',
+    'box-shadow':'0 6px 8px #00000030'
+  })
   
-    return array;
+  btn.onclick = (e)=> {
+    dir = (i==0)? 1:-1
+    if (i==0) {
+      currentCard--
+      currentCard = Math.max(0, currentCard)
+    }
+    else {
+      currentCard++
+      currentCard = Math.min(4, currentCard)
+    }
+    moveCards(0.75)
   }
-
-
-if (cookie) {
-    let data = cookie.split('/')
-    container_start_h3.innerHTML = `<h3>В прошлый раз вы дали ${data[1]} правильных ответов из ${data[0]}. Точность - ${Math.round(data[1] * 100 / data[0])}%.</h3>`
-}
-
-function randint(min, max) {
-    return Math.round(Math.random() * (max - min) + min)
-}
-
-function getRandomSign() {
-    return signs[randint(0, 2)]
-}
-
-class Question {
-    constructor() {
-        let a = randint(1, 30)
-        let b = randint(1, 30)
-        let sign = getRandomSign()
-        this.question = `${a} ${sign} ${b}`
-        if (sign == '+') { this.correct = a + b }
-        else if (sign == '-') { this.correct = a - b }
-        else if (sign == '*') { this.correct = a * b }
-        this.answers = [
-            randint(this.correct - 20, this.correct - 1),
-            randint(this.correct - 20, this.correct - 1),
-            this.correct,
-            randint(this.correct + 1, this.correct + 20),
-            randint(this.correct + 1, this.correct + 20),
-        ]
-        shuffle(this.answers);
-    }
-
-    display () {
-        question_field.innerHTML = this.question
-        for (let i = 0; i < this.answers.length; i += 1) {
-            answer_buttons[i].innerHTML = this.answers[i]
-        }
-    }
-}
-
-let current_question
-let correct_answers_given
-let total_answers_given
-start_button.addEventListener('click', function() {
-    container_main.style.display = 'flex'
-    container_start.style.display = 'none'
-    current_question = new Question()
-    current_question.display()
-
-    correct_answers_given = 0
-    total_answers_given = 0
-
-    setTimeout(function() {
-        let new_cookie = `numbers_score=${total_answers_given}/${correct_answers_given}; max-age=10000000000`
-        document.cookie = new_cookie
-        
-container_main.style.display = 'none'
-container_start.style.display = 'flex'
-container_start_h3.innerHTML = `<h3>Вы дали ${correct_answers_given} правильных ответов из ${total_answers_given}. Точность - ${Math.round(correct_answers_given * 100 / total_answers_given)}%.</h3>`
-    }, 10000)
 })
 
-for (let i = 0; i < answer_buttons.length; i += 1) {
-    answer_buttons[i].addEventListener('click', function() {
-        if (answer_buttons[i].innerHTML == current_question.correct) {
-            correct_answers_given += 1
-            answer_buttons[i].style.background = '#00FF00'
-            anime({
-                targets: answer_buttons[i],
-                background: '#FFFFFF',
-                duration: 500,
-                delay: 100,
-                easing: 'linear'
-            })
-        } else {
-            answer_buttons[i].style.background = '#FF0000'
-            anime({
-                targets: answer_buttons[i],
-                background: '#FFFFFF',
-                duration: 500,
-                delay: 100,
-                easing: 'linear'
-            })
-        }
-        total_answers_given += 1
+cardBtns.forEach((btn,i)=>{
+  btn.onpointerenter = (e)=> gsap.to(btn, {
+    ease:'power3',
+    overwrite:'auto',
+    'box-shadow':()=>(i==currentCard)?'0 6px 11px #00000030':'0 6px 11px #00000030'
+  })
+  
+  btn.onpointerleave = (e)=> gsap.to(btn, {
+    ease:'power3',
+    overwrite:'auto',
+    'box-shadow':()=>(i==currentCard)?'0 6px 11px #00000030':'0 0px 0px #00000030'
+  })
 
+  btn.onclick = (e)=> {
+    dir = (i<currentCard)? 1:-1
+    currentCard = i
+    moveCards(0.75)
+  }
+})
 
-        current_question = new Question()
-        current_question.display()
-    })
+function moveCards(dur=0){
+  gsap.timeline({defaults:{ duration:dur, ease:'power3', stagger:{each:-0.03*dir} }})
+    .to('.card', {
+      x:-270*currentCard,
+      y:(i)=>(i==currentCard)?0:15,
+      height:(i)=>(i==currentCard)?270:240,
+      ease:'elastic.out(0.4)'
+    }, 0)
+    .to('.card', {
+      cursor:(i)=>(i==currentCard)?'default':'pointer',
+      'box-shadow':(i)=>(i==currentCard)?'0 6px 11px #00000030':'0 0px 0px #00000030',
+      border:(i)=>(i==currentCard)?'2px solid #26a':'0px solid #fff',
+      background:(i)=>(i==currentCard)?'radial-gradient(100% 100% at top, #fff 0%, #fff 99%)':'radial-gradient(100% 100% at top, #fff 20%, #eee 175%)',
+      ease:'expo'
+    }, 0)
+    .to('.icon svg', {
+      attr:{
+        stroke:(i)=>(i==currentCard)?'transparent':'#36a',  
+        fill:(i)=>(i==currentCard)?'#36a':'transparent'
+      }
+    }, 0)
+    .to('.arrow-btn-prev', {
+      autoAlpha:(currentCard==0)?0:1
+    }, 0)
+    .to('.arrow-btn-next', {
+      autoAlpha:(currentCard==4)?0:1
+    }, 0)
+    .to('.card h4', {
+      y:(i)=>(i==currentCard)?0:8,    
+      opacity:(i)=>(i==currentCard)?1:0,
+    }, 0)
 }
